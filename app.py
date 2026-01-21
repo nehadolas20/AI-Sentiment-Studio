aceca34ad32b390015d32c9974eb05f7e83279a1
nehadolas20 <dolasneha.20@gmail.com>
2026-01-21 20:43:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,4 +116,8 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
